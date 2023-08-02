7c3b76ec540dfd30d163a9c158e0b39d008d573f
Matthew Smailey <them4tthew@gmail.com>
2023-08-02 13:52:48 -0700

    fixed log file append issue

--- log.go
+++ log.go
@@ -104,12 +104,19 @@ func LogRaw(message Message) {
 }
 
 func LogToFile(text, filename string) error {
-
-    // Write the text to the file
-    err := ioutil.WriteFile(filename, []byte(text), 0644)
+    // Open the file in append mode, creating it if it doesn't exist
+    file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
         return err
     }
+    defer file.Close()
+
+    // Write the text to the file
+    _, err = file.WriteString(text)
+    if err != nil {
+        return err
+    }
+
     return nil
 }
 
